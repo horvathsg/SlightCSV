3639f72e19c85ade5a4ce63e7be117a427b1d045
Simon Horvath <horvathsg@gmail.com>
2019-03-26 11:52:45 +0100

    Separated tests into class-related test suites.

--- test/test_slightmatrix.cpp
+++ test/test_slightmatrix.cpp
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include "test_include.hpp"
+
 TEST_GROUP(slightmatrix) {
 };
 
